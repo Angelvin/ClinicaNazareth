c270a5a6e5cc60fc46e57ea4ada5f7c44affb873
Jose Guillen <damgel@outlook.com>
2014-06-25 11:16:23 -0600

    renamed variables.

--- src/java/DAL/cConexion.java
+++ src/java/DAL/cConexion.java
@@ -13,15 +13,15 @@ import java.sql.*;
 public class cConexion {
 
     private static final String SQLSERVER_URL = "jdbc:sqlserver://localhost:49759;databaseName=clinica";
-    private static final String MYSQL_USER = "sa";
-    private static final String MYSQL_PASSWD = "destiny";
+    private static final String SQL_USER = "sa";
+    private static final String SQL_PASSWD = "*******";
 
     public static Connection conectar_ds() {
         Connection cnn = null;
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            cnn = DriverManager.getConnection(SQLSERVER_URL, MYSQL_USER, MYSQL_PASSWD);
-            System.out.println("Conectado a :" + SQLSERVER_URL);
+            cnn = DriverManager.getConnection(SQLSERVER_URL, SQL_USER, SQL_PASSWD);
+            System.out.println("Coneccion exitosa a :" + SQLSERVER_URL);
         } catch (SQLException ex) {
             System.out.println("Error al conectarse a la base de datos: " + ex.getMessage());
         } finally {
