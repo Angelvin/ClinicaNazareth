a349575af126c809f799b570676bc839c28c9fb3
Jose Guillen <damgel@outlook.com>
2014-07-10 15:44:15 -0600

    correccion en la query

--- src/java/BAL/Bcitamedico.java
+++ src/java/BAL/Bcitamedico.java
@@ -13,82 +13,84 @@ import java.util.*;
  * @author Angel
  */
 public class Bcitamedico {
-    
+
     private int idcita;
     private String motivo;
     private String horario;
     private String paciente;
     private int codipaciente;
-    
+
     public int getCodipaciente() {
         return codipaciente;
     }
-    
+
     public void setCodipaciente(int codipaciente) {
         this.codipaciente = codipaciente;
     }
-    
+
     public int getIdcita() {
         return idcita;
     }
-    
+
     public void setIdcita(int idcita) {
         this.idcita = idcita;
     }
-    
+
     public String getMotivo() {
         return motivo;
     }
-    
+
     public void setMotivo(String motivo) {
         this.motivo = motivo;
     }
-    
+
     public String getHorario() {
         return horario;
     }
-    
+
     public void setHorario(String horario) {
         this.horario = horario;
     }
-    
+
     public String getPaciente() {
         return paciente;
     }
-    
+
     public void setPaciente(String paciente) {
         this.paciente = paciente;
     }
-    
+
     public String getMedico() {
         return medico;
     }
-    
+
     public void setMedico(String medico) {
         this.medico = medico;
     }
-    
+
     public static String getCMedico() {
         return CMedico;
     }
-    
+
     public static void setCMedico(String CMedico) {
         Bcitamedico.CMedico = CMedico;
     }
     private String medico;
-    public static String CMedico = "select top 1 (c.idCita) as idcita,(c.motivo) as motivo,(paci.idPaciente) as codipaciente,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer) as  paciente, concat('Dr. ',per2.pnombrePer, ' ',per2.pApellPer) as medico  from cita as c inner join horario as h on c.fkhorario=h.idhorario inner join paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as per on per.idPersona=paci.fkpersona inner join empleado as emp on emp.idEmpleado=h.fkempleado inner join persona as per2 on per2.idPersona=emp.fkpersona where (estadoCita='Consulta') and (fechaCita=CAST(CURRENT_TIMESTAMP AS DATE)) and emp.idEmpleado=(select idEmpleado  from persona as p inner join login as l on l.idLogin=p.fkLogin inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin=";
-    
+    public static String CMedico = "select  (c.idCita) as idcita,(c.motivo) as motivo,(paci.idPaciente) as codipaciente,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer) as  paciente, concat('Dr. ',per2.pnombrePer, ' ',per2.pApellPer) as medico  from cita as c inner join horario as h on c.fkhorario=h.idhorario inner join paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as per on per.idPersona=paci.fkpersona inner join empleado as emp on emp.idEmpleado=h.fkempleado inner join persona as per2 on per2.idPersona=emp.fkpersona where (estadoCita='Consulta')  and emp.idEmpleado=(select idEmpleado  from persona as p inner join login as l on l.idLogin=p.fkLogin inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin=";
+
     public List< Bcitamedico> getlistado(Integer valor) {
         String query = CMedico + valor + ")";
         List< Bcitamedico> lista = new ArrayList< Bcitamedico>(0);
         Connection cnn = cConexion.conectar_ds();
         Statement ts;
         ResultSet rs;
-        
-        try {
+
+        try
+        {
             ts = cnn.createStatement();
             rs = ts.executeQuery(query);
-            while (rs.next()) {
+            while (rs.next())
+            {
                 Bcitamedico Cm = new Bcitamedico();
                 Cm.setIdcita(rs.getInt("idcita"));
                 Cm.setCodipaciente(rs.getInt("codipaciente"));
@@ -97,18 +99,20 @@ public class Bcitamedico {
                 Cm.setPaciente(rs.getString("paciente"));
                 Cm.setMedico(rs.getString("medico"));
                 lista.add(Cm);
-                
+
             }
             cnn.close();
             ts.close();
             rs.close();
-            
-        } catch (NullPointerException ex) {
+
+        } catch (NullPointerException ex)
+        {
             System.out.println("Truena el Controlador: " + ex.getMessage());
-        } finally {
+        } finally
+        {
             return lista;
         }
-        
+
     }
     /*
      * public static void main(String[] args) { Connection bdconeccion =
