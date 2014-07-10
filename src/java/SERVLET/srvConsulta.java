5e14809ab6f09e5932c42dee90aa42109b5ec035
angel <jaga_jangel@hotmail.com>
2014-07-10 16:19:11 +0200

    archivo es servlet con persistencia

---
+++ src/java/SERVLET/srvConsulta.java
@@ -0,0 +1,111 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SERVLET;
+
+import CJPA.Cita;
+import CJPA.Consulta;
+import CJPA.DetalleMedicina;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Marlon
+ */
+public class srvConsulta extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP
+     * <code>GET</code> and
+     * <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        EntityManagerFactory emf=Persistence.createEntityManagerFactory("primerejemploPU");
+        EntityManager em=emf.createEntityManager();
+        try {
+            Consulta consul=new Consulta();
+            Cita cta=new Cita();
+            consul.setPeso(request.getParameter("txtpeso"));
+            consul.setTemperatura(request.getParameter("txttemperatura"));
+            consul.setTensionalterial(request.getParameter("txttensionarterial"));
+            consul.setConsultaPor(request.getParameter("txtconsultapor"));
+            consul.setPulso(request.getParameter("txtpulso"));
+            consul.setTipoConsulta(request.getParameter("txttipoconsulta"));
+            consul.setExamenes(request.getParameter("txtexamen"));
+            consul.setSintomasConsulta(request.getParameter("txtsintomas"));
+            consul.setFkcita(em.find(Cita.class, Integer.parseInt(request.getParameter(""))));
+            consul.setFkDetMedTrata(DetalleMedicina.class,Integer.parseInt(request.getParameter()));
+         
+            //agrega consulta
+            em.getTransaction().begin();
+            em.persist(consul);
+            em.getTransaction().commit();
+            //actualiza cita
+            cta=em.find(Cita.class,Integer.parseInt(request.getParameter("txtIDCita")));
+            cta.setEstadoCita("Finalizado");
+            em.getTransaction().begin();
+            em.merge(cta);
+            em.getTransaction().commit();
+            
+        } catch (Exception e) {
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
