abddf30d8fde1613b183deb04518f01646f0c22c
Jose Guillen <damgel@outlook.com>
2014-07-10 17:29:05 -0600

    concatenacion de query arreglada

--- src/java/BAL/Bcitamedico.java
+++ src/java/BAL/Bcitamedico.java
@@ -4,12 +4,9 @@
  */
 package BAL;
 
-import DAL.cConexion;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
+import DAL.*;
+import java.sql.*;
+import java.util.*;
 
 /**
  *
@@ -70,20 +67,21 @@ public class Bcitamedico {
         Bcitamedico.CMedico = CMedico;
     }
     private String medico;
-    public static String CMedico = "select top 1 (c.idCita) as idcita,(c.motivo) as motivo,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer) as  paciente, concat('Dr. ',per2.pnombrePer, ' ',per2.pApellPer) as medico  from cita as c inner join horario as h on c.fkhorario=h.idhorario inner join paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as per on per.idPersona=paci.fkpersona inner join empleado as emp on emp.idEmpleado=h.fkempleado inner join persona as per2 on per2.idPersona=emp.fkpersona where (estadoCita='Consulta') and (fechaCita=CAST(CURRENT_TIMESTAMP AS DATE)) and emp.idEmpleado=(select idEmpleado  from persona as p inner join login as l on l.idLogin=p.fkLogin inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin= ) ";
-    int valor = 2;
+    public static String CMedico = "select top 1 (c.idCita) as idcita,(c.motivo) as motivo,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer) as  paciente, concat('Dr. ',per2.pnombrePer, ' ',per2.pApellPer) as medico  from cita as c inner join horario as h on c.fkhorario=h.idhorario inner join paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as per on per.idPersona=paci.fkpersona inner join empleado as emp on emp.idEmpleado=h.fkempleado inner join persona as per2 on per2.idPersona=emp.fkpersona where (estadoCita='Consulta') and (fechaCita=CAST(CURRENT_TIMESTAMP AS DATE)) and emp.idEmpleado=(select idEmpleado  from persona as p inner join login as l on l.idLogin=p.fkLogin inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin=";
 
     public List< Bcitamedico> getlistado(Integer valor) {
-        String query = CMedico + valor;
+        String query = CMedico + valor + ")";
         List< Bcitamedico> lista = new ArrayList< Bcitamedico>(0);
         Connection cnn = cConexion.conectar_ds();
         Statement ts;
         ResultSet rs;
 
-        try {
+        try
+        {
             ts = cnn.createStatement();
             rs = ts.executeQuery(query);
-            while (rs.next()) {
+            while (rs.next())
+            {
                 Bcitamedico Cm = new Bcitamedico();
                 Cm.setIdcita(rs.getInt("idcita"));
                 Cm.setMotivo(rs.getString("motivo"));
@@ -97,45 +95,47 @@ public class Bcitamedico {
             ts.close();
             rs.close();
 
-
-
-
-        } catch (NullPointerException ex) {
+        } catch (NullPointerException ex)
+        {
             System.out.println("Truena el Controlador: " + ex.getMessage());
-        } finally {
+        } finally
+        {
             return lista;
         }
 
-
-
     }
-    /* public static void main(String[] args) {
-     Connection bdconeccion = cConexion.conectar_ds();
-     CallableStatement buscador = null;
-     ResultSet rs = null;
-     Bfecha f = new Bfecha();
-     Statement stmt = null;
-
-     try {
-     stmt = bdconeccion.createStatement();
-
-
-     rs = stmt.executeQuery("select top 1 (c.idCita) as idcita,(c.motivo) as motivo,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer) as  paciente, concat(per2.pnombrePer, ' ',per2.pApellPer) as medico  from cita as c inner join horario as h on c.fkhorario=h.idhorario inner join paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as per on per.idPersona=paci.fkpersona inner join empleado as emp on emp.idEmpleado=h.fkempleado inner join persona as per2 on per2.idPersona=emp.fkpersona where (estadoCita='Consulta') and (fechaCita=CAST(CURRENT_TIMESTAMP AS DATE)) and emp.idEmpleado=(select idEmpleado  from persona as p inner join login as l on l.idLogin=p.fkLogin inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin=" + 2 + ")");
-
-
-     while (rs.next()) {
-     System.out.println(rs.getInt("idcita"));
-     System.out.println(rs.getString("motivo"));
-     System.out.println(rs.getString("horario"));
-     System.out.println(rs.getString("paciente"));
-     System.out.println(rs.getString("medico"));
-     }
-
-
-
-     } catch (SQLException e) {
-     System.out.println(e.getMessage());
-     }
-
-     }*/
+    /*
+     * public static void main(String[] args) { Connection bdconeccion =
+     * cConexion.conectar_ds(); CallableStatement buscador = null; ResultSet rs
+     * = null; Bfecha f = new Bfecha(); Statement stmt = null;
+     *
+     * try { stmt = bdconeccion.createStatement();
+     *
+     *
+     * rs = stmt.executeQuery("select top 1 (c.idCita) as idcita,(c.motivo) as
+     * motivo,(h.horaini) as horario,concat(per.pnombrePer, ' ',per.pApellPer)
+     * as paciente, concat(per2.pnombrePer, ' ',per2.pApellPer) as medico from
+     * cita as c inner join horario as h on c.fkhorario=h.idhorario inner join
+     * paciente as paci on c.fkPaciente=paci.idPaciente inner join persona as
+     * per on per.idPersona=paci.fkpersona inner join empleado as emp on
+     * emp.idEmpleado=h.fkempleado inner join persona as per2 on
+     * per2.idPersona=emp.fkpersona where (estadoCita='Consulta') and
+     * (fechaCita=CAST(CURRENT_TIMESTAMP AS DATE)) and emp.idEmpleado=(select
+     * idEmpleado from persona as p inner join login as l on l.idLogin=p.fkLogin
+     * inner join empleado as e on e.fkpersona=p.idPersona where l.idLogin=" + 2
+     * + ")");
+     *
+     *
+     * while (rs.next()) { System.out.println(rs.getInt("idcita"));
+     * System.out.println(rs.getString("motivo"));
+     * System.out.println(rs.getString("horario"));
+     * System.out.println(rs.getString("paciente"));
+     * System.out.println(rs.getString("medico")); }
+     *
+     *
+     *
+     * } catch (SQLException e) { System.out.println(e.getMessage()); }
+     *
+     * }
+     */
 }
