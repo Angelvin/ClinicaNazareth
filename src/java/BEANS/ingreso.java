2ef4594f7453f709ef413839a0cb2263e484d09e
Jose Guillen <damgel@outlook.com>
2014-07-12 04:48:05 -0600

    cambie a query en vez del procedure porque aun no funciona correctamente

--- src/java/BEANS/ingreso.java
+++ src/java/BEANS/ingreso.java
@@ -11,7 +11,7 @@ package BEANS;
 public class ingreso {
 
     public static String verificar = "SELECT count(*) as registros FROM login where usuario=?";
-    /*public static String session = "SELECT fkRol, idLogin, usuLogin FROM login where usuLogin=? and passLogin=?";*/
+    public static String session = "SELECT fkRol, idLogin, usuLogin FROM login where usuLogin=? and passLogin=?";
     public static String Ingreso = "insert into entrada(cantidadentrada,presentacionEntrada,precioEntrada,fkPreducto)values(?,?,?,?)";
-    public static String session = "{call session(?,?)}";
+    //public static String session = "{call session(?,?)}";
 }
