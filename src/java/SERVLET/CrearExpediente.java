0f45242d421af11c645d85be4e2ffb81ad8b842e
Jose Guillen <damgel@outlook.com>
2014-07-11 23:43:44 -0600

    insert consulta implementado

--- src/java/SERVLET/CrearExpediente.java
+++ src/java/SERVLET/CrearExpediente.java
@@ -30,11 +30,14 @@ public class CrearExpediente extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
-        processRequest(request, response);
 
-        int idpaciente = Integer.parseInt(request.getParameter("codigoCita"));
+        int PacienteID = Integer.parseInt(request.getParameter("txtPacienteID"));
+        int EmpleadoID = Integer.parseInt(request.getParameter("txtEmpleadoID"));
 
-        String InsertExpediente = "";
+        out.println("Valores en request: Paciente->" + PacienteID + "Empleado ->" + EmpleadoID);
+
+        String InsertExpediente = "INSERT INTO expediente(fkEmpleado, fkpaciente)values(";
+        InsertExpediente = InsertExpediente + PacienteID + "," + EmpleadoID + ")";
         PreparedStatement pstm;
         Connection cnn = cConexion.conectar_ds();
         int returnVal;
@@ -48,18 +51,17 @@ public class CrearExpediente extends HttpServlet {
             Logger.getLogger(Paciente.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-}
 
-// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-/**
- * Handles the HTTP <code>GET</code> method.
- * @param request servlet request
- * @param response servlet response
- * @throws ServletException if a servlet-specific error occurs
- * @throws IOException if an I/O error occurs
- */
-@Override
-        protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -72,7 +74,7 @@ public class CrearExpediente extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         processRequest(request, response);
     }
@@ -82,7 +84,7 @@ public class CrearExpediente extends HttpServlet {
      * @return a String containing servlet description
      */
     @Override
-        public String getServletInfo() {
+    public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
